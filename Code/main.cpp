20aea19b9595a276ddbb3e622f5b4ac50d6838f5
KuKuXia <liuxiaolongsdu@mail.sdu.edu.cn>
2019-01-14 22:40:44 +0800

    Up to C8-Q10

---
+++ Code/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Forward_List.h"
+#include "cstdio"
+int main() {
+    using namespace forward_list;
+//    node *head = create();
+//    printf("Length: %d\n", length(head));
+//
+//    head = insert_node(head, 2, 5);
+//    printf("Insert integer 5 after 2th node:\n");
+//    print(head);
+//    head = delete_node(head, 2);
+//
+//    printf("Delete the 3th node:\n");
+//    print(head);
+//
+//    reverse(head);
+//    printf("Reverse the link:\n");
+//    print(head);
+//
+//    search_middle(head);
+//    printf("Find the middle element:\n");
+//    node *middle = search_middle(head);
+//    std::cout << middle->data << std::endl;
+//    node *head1 = InsertSort();
+//    print(head1);
+   test_IsLoop();
+    return 0;
+}
